61ad9c0b45dce259058c2ddb29050007fe9bcd7f
Mayfield <waltmayf@amazon.com>
2022-02-02 10:09:26 -0600

    Where is this going

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 Iakov has already made a wonderful slots tool: https://github.com/iakov-aws/slots
 
+# Slots
+
+## About
+
+This tool shows all slots where attendees are free. It uses Microsoft Outlook to read agenda. Windows and Mac are supported
+
+## Prerequisites
+* mac or windows
+* python3.9+
+* Microsoft Outlook
+
+
+## Example
+```
+python3 slots.py  --attendees 'attendee1@domain.com; Atten Dee <attendee2@domain>' -tz 'America/New_York'
+? Select a Timeslot to create a meeting:
+  Cancel (CTRL+C)
+  ---------------
+  Thursday 20 January 14:00 - 15:00 CET / 08:00 - 09:00 America/New_York
+  Thursday 20 January 16:00 - 18:00 CET / 10:00 - 12:00 America/New_York
+  Friday 21 January 14:00 - 15:00 CET / 08:00 - 09:00 America/New_York
+ >Friday 21 January 15:30 - 18:00 CET / 09:30 - 12:00 America/New_York
+  Monday 24 January 15:00 - 18:00 CET / 09:00 - 12:00 America/New_York
+  Tuesday 25 January 15:00 - 18:00 CET / 09:00 - 12:00 America/New_York
+  Wednesday 26 January 16:00 - 18:00 CET / 10:00 - 12:00 America/New_York
+  Thursday 27 January 14:00 - 15:00 CET / 08:00 - 09:00 America/New_York
+
+```
+
+
+## Install
+### mac
+```
+pip install -r requirements/macos.txt
+```
+### win
+```
+pip install -r requirements/windows.txt
+```
+
+
+## Advanced usage
+Show all slots where 5O% of attendees are free and show alternative timezone:
+```
+python3 slots.py  --attendees 'attendee1@domain.com; Atten Dee <attendee2@domain>' --rate 50 -tz America/New_York
+```
+
+Show full agenda:
+```
+python3 slots.py  --attendees 'attendee1@domain.com; Atten Dee <attendee2@domain>' --rate 50  --full
+```
+
+More:
+```
+❯ python3 slots.py --help
+Usage: slots.py [OPTIONS]
+
+Options:
+  -a, --attendees TEXT          A semicolon separated list of contacts
+  --start TEXT                  Begining of the date range to find available
+                                slots. Iso date or Arrow Humanised time (ex:
+                                "in 30 days"). See https://arrow.readthedocs.io/en/latest/#dehumanize. 
+                                Default is today.
+  --end TEXT                    The end of the date range to find available
+                                slots. Iso date or Arrow Humanised time (ex:
+                                "in 30 days"). See https://arrow.readthedocs.io/en/latest/#dehumanize. 
+                                Default is "in 7
+                                days".
+  --full / --only-slots         Show slots or a full agenda (default=only
+                                slots)
+  -r, --rate INTEGER            Acceptable share of available attendees in
+                                percent(%)
+  --tentative / --no-tentative  Treat tentative meetings as free
+  -l, --lenght INTEGER          Length of the meeting, in minutes 
+  -tz, --alternative_tz TEXT    A comma separated list of alternative
+                                Timezones. if "auto" script will fetch
+                                timezones from slack (mwinit --aea requied).
+  -hr, --hours TEXT             Find availability between the hours of...
+                                Default = "0800-1900".
+  -f, --fmt TEXT                Time format in list of available slots.
+                                "HH:mm" or "h:mma". Refer to https://arrow.rea
+                                dthedocs.io/en/latest/#supported-tokens.
+                                Default "HH:mm".
+  --help                        Show this message and exit.
+ ```
---
+++ slots.py
@@ -0,0 +1,424 @@
+""" A script that finds meeting slots in outlook
+"""
+import sys
+import getpass
+import datetime
+import zoneinfo
+from dateutil import tz
+
+import click
+import arrow
+from InquirerPy import prompt
+from InquirerPy.separator import Separator
+from InquirerPy import inquirer
+from InquirerPy.prompts.checkbox import CheckboxPrompt
+
+LOCAL_TIMEZONE = datetime.datetime.now().astimezone().tzinfo
+
+class OutlookMac():
+    def __init__(self):
+        from appscript import app, k
+        self.outlook = app('Microsoft Outlook')
+        self.k = k
+
+    def get_freebusy(self, attendee, start_time, end_time, interval=15):
+        """ check freebusy status in all accounts
+        """
+
+        start_time = arrow.get(start_time)
+        end_time = arrow.get(end_time)
+        for account in self.outlook.exchange_account(): 
+            try:
+                res = self.outlook.query_freebusy ( 
+                    account,
+                    for_attendees=[attendee],
+                    range_start_time=start_time.naive,
+                    range_end_time=end_time.naive,
+                    interval=interval,
+                )
+            except:
+                continue
+            visibilitiy = {}
+            attendee_email = res.pop(0)
+            current_time = arrow.get(res.pop(0), 'YYYY-MM-DD HH:mm:ss Z')
+            while  current_time < end_time:
+                if len(res) == 0: break
+                name, location, status =  (res.pop(0), res.pop(0), res.pop(0))
+                visibilitiy[current_time] = name, location, status
+                current_time = current_time.shift(minutes=interval)
+
+            variations = set(list(visibilitiy.values()))
+            if variations == {('', '', 'no info')}:
+                continue 
+            break
+        else:
+            raise Exception("Not found %r in %s accounts. Check email, Try restart Outlook or use VPN" % (attendee, len(outlook.exchange_account()) ))
+
+        return visibilitiy
+
+    def create_event(self, subject, content, start_time, end_time, attendees):
+        event = self.outlook.make(
+            new=self.k.calendar_event,
+            with_properties={
+                self.k.subject: subject,
+                self.k.content: content,
+                self.k.free_busy_status: self.k.busy,
+                self.k.start_time: datetime.datetime(start_time.year, start_time.month, start_time.day, start_time.hour, start_time.minute), 
+                self.k.end_time: datetime.datetime(end_time.year, end_time.month, end_time.day, end_time.hour, end_time.minute), 
+            },
+        )
+        for email in attendees or []:
+            event.make(
+                new=self.k.required_attendee,
+                with_properties={self.k.email_address: {self.k.address: email}}
+            )
+        event.open()
+        event.activate()
+
+        return event
+
+#https://docs.microsoft.com/en-us/office/vba/api/outlook.exchangeuser.getfreebusy
+class OutlookWin():
+    def __init__(self):
+        import win32com.client # pip install pywin32
+        self.outlook = win32com.client.Dispatch('Outlook.Application')
+        self.namespace = self.outlook.GetNamespace("MAPI")
+
+    def get_freebusy(self, attendee, start_time, end_time, interval=15):
+        """ check freebusy status in all accounts
+            This is broken because the function outputs times starting at midnight. The arrow default starts at 11pm.
+            https://docs.microsoft.com/en-us/office/vba/api/outlook.exchangeuser.getfreebusy
+
+            Might need to correct this for timezone too.
+            Looks like the "Office" field is populated. I could look up the airport code to find timezone.
+            https://raw.githubusercontent.com/hroptatyr/dateutils/tzmaps/iata.tzmap
+        """
+        saved_args = locals()
+        print("get_freebusy saved_args is", saved_args)
+
+        timezone = 'US/Pacific'
+
+        recipient = self.namespace.CreateRecipient(attendee)
+        start_time = arrow.get(start_time).replace(hour=0,minute=0, second=0).to('UTC')
+        # start_time = arrow.get(datetime.datetime(2013, 5, 5), 'US/Pacific')
+        end_time = arrow.get(end_time)
+        res = recipient.FreeBusy(start_time.datetime, interval, True)
+
+        print(f'freeBusy start_time: {start_time}')
+        # print(f'start_time as datetime: {start_time.datetime}')
+        # print(f'####### res: {res}  ########')
+
+        visibilitiy = {}
+        current_time = start_time
+        for item in res:
+            status = {
+                '4': 'oof', #olWorkingElsewhere
+                '3': 'oof',
+                '2': 'busy',
+                '1': 'tentative',
+                '0': 'free',
+            }.get(item, 'no info')
+
+            if current_time > end_time: break #Don't consider times past the end time
+
+            #Mark Busy if outside working hours
+            attendeeTime = current_time.to(timezone)
+            if attendeeTime.format('HHmm') > '1700' or attendeeTime.format('HHmm') < '0800': status = 'busy'
+            if attendeeTime.format('ddd') in ("Sat", "Sun"): status = 'busy'
+            
+            visibilitiy[current_time] = attendee, '', status
+            current_time = current_time.shift(minutes=interval)
+        return visibilitiy    
+
+    def create_event(self, subject, content, start_time, end_time, attendees):
+        
+        # str_start_time = start_time.replace(hour=0,minute=0, second=0).format()
+        print('event creation is not yet implemented for windows')
+        mail = self.outlook.CreateItem(1)   #Open new mail
+
+        mail.Start = str_start_time  #Append start time to the 
+        mail.Subject = subject
+        mail.Duration = 15 #Duration in minutes. Do the math with the start and end time.
+        mail.MeetingStatus = 1 
+
+        for alias in attendees: mail.Recipients.Add(alias)
+        mail.Display()
+
+        return None
+
+@click.command()
+@click.option('-a', '--attendees', default=None, help='A semicolon separated list of contacts')
+@click.option('--start', default='today', help='Begining of the date range to find available slots. Iso date or Arrow Humanised time (ex: "in 30 days"). See https://arrow.readthedocs.io/en/latest/#dehumanize. Default is today. ')
+@click.option('--end', default='in 30 days',
+              help='The end of the date range to find available slots. Iso date or Arrow Humanised time (ex: "in 30 days"). See https://arrow.readthedocs.io/en/latest/#dehumanize. Default is "in 30 days".')
+@click.option('--full/--only-slots', default=False, help='Show slots or a full agenda (default=only slots)')
+@click.option('-r', '--rate', default=100, help='Acceptable share of available attendees in percent(%)')
+@click.option('--tentative/--no-tentative', default=True, help="Treat tentative meetings as free")
+@click.option('-l', '--lenght', default=60, help='Length of the meeting, in minutes. default=60')
+@click.option('-tz', '--alternative_tz', default=None, help='A comma separated list of alternative Timezones.')
+@click.option('-hr', '--hours', default='0800-1700', help='Find availability between the hours of... Default = "0800-1900". ')
+@click.option('-f', '--fmt', default='HH:mm',
+              help='Time format in list of available slots. "HH:mm" or "h:mma". Refer to https://arrow.readthedocs.io/en/latest/#supported-tokens. Default "HH:mm".')
+def main(attendees, start, end, full, rate, lenght, tentative, alternative_tz, hours, fmt):
+    saved_args = locals()
+    print("saved_args is", saved_args)
+
+    if start == 'today':
+        start = arrow.now('US/Central').replace(minute=0, second=0)
+    else:
+        try:
+            start = arrow.get('US/Central').dehumanize(start).replace(minute=0, second=0)
+        except:
+            pass 
+
+    print(f'Initial Start: {start}')
+
+    between_lower, between_upper = hours.split('-')
+
+    new_end = None
+    try: 
+        new_end = arrow.get(end)
+    except:
+        try:
+            new_end = arrow.get().dehumanize(end).replace(minute=0, second=0)
+        except:
+            pass 
+    assert new_end, f"I do not understand --end='{end}'. Please use ISO date or ARROW Humanise syntax. ex: 'in 30 days'"
+    end = new_end
+
+    if not attendees:
+        answer = inquirer.text(
+            message="enter attendees",
+            long_instruction='Semicolon `;` or new line `\\n` separated list. ',
+            instruction=' (Use `esc` followd by `enter` to complete the question.)',
+            multiline=True,
+            default=''
+        ).execute()
+        attendees = answer.replace('\n', ";")
+
+    new_attendees = []
+    for addr in attendees.split(";"):
+        addr = addr.strip()
+        if not addr: continue
+        if '<' in addr and '>' in addr:
+            addr = addr.split('<')[1].split('>')[0]
+            new_attendees.append(addr)
+        else:
+           new_attendees.append(addr)
+    attendees = list(set(new_attendees))
+
+    
+    alternative_tz = alternative_tz.split(',') if alternative_tz else []
+
+    # print(f'########## alternative_tz: {alternative_tz} ##########')
+    
+    ### This part breaks it for me a looks like a safety check. Delete for now.
+    # for itz, tz in enumerate(alternative_tz): 
+    #     print(f'########## tz: {tz}, Available Time Zones {zoneinfo.available_timezones()}')
+    #     if tz not in zoneinfo.available_timezones():
+    #         alternative_tz[itz] = inquirer.fuzzy(
+    #             message="Cannot find the TZ. Please select timezone:",
+    #             choices=sorted(zoneinfo.available_timezones()),
+    #             default=tz,
+    #         ).execute()
+
+    print ('Looking up agendas for', '; '.join(attendees))
+
+    if sys.platform == 'darwin':
+        outlook = OutlookMac()
+    elif sys.platform.startswith('win'):
+        outlook = OutlookWin()
+    else:
+        raise NotImplementedError(f'{sys.platform} not supported')
+
+    interval = 15 if lenght <=30 else 30 
+    freebusy = {}
+    for index, _ in enumerate(attendees):
+        while True: # for retry
+            attendee = attendees[index]
+            try:
+                freebusy[attendee] = outlook.get_freebusy(attendee, start, end, interval=interval)
+                break
+            except Exception as exc:
+                print(exc)
+                answer = inquirer.text(
+                    message=f"Cannot find {attendee} in outlook. Please correct email or delete to skip",
+                    long_instruction='',
+                    default=attendee
+                ).execute()
+                if not answer: 
+                    freebusy[attendee] = None
+                    break
+                attendees[index] = answer
+
+    freebusy = dict([item for item in freebusy.items() if item[1] ]) #filter empty 
+    
+    # print(f'######## freebusy: {freebusy.replace()}')
+    # print("{" + "\n".join("{!r}: {!r},".format(k, v) for k, v in freebusy.items()) + "}")
+
+    print('####### Free Busy Times ########')
+    for k, v in freebusy['waltmayf@amazon.com'].items(): 
+        # print('Hello World')
+        print(f'{k} {v}')
+
+
+    current_time_start = None
+    current_slot_status = 'not started'
+    current_busy_attendees = []
+
+    busy_statuses = ['oof', 'busy'] if tentative else ['oof', 'busy', 'tentative']
+
+    # candidateTimes = list(freebusy.values())[0]
+    
+    # dFreePeople={}
+    # for time in candidateTimes:
+    #     dFreePeople[time]=sum([(freebusy[att][time][2] not in busy_statuses) for att in freebusy])
+
+    # print("###### dFreePeople #######")
+    # for k, v in dFreePeople.items(): 
+    #     # print('Hello World')
+    #     print(f'{k} {v}')
+    
+    # dFreePeople = {time:sum([freebusy[att][time][2] not in busy_statuses])for time in candidateTimes}
+
+    # if freebusy[att][time][2] in busy_statuses: free = False
+
+    if full:
+        # show all slots
+        choices = []
+        last_date = ''
+        for time in list(freebusy.values())[0]:
+            line  = time.format('    HH:mm ')
+            if time.format('HHmm') > between_upper or time.format('HHmm') < between_lower: continue
+            if time.format('ddd') in ("Sat", "Sun"): continue
+            if time.format("dddd DD MMMM") != last_date:
+                last_date = time.format("dddd DD MMMM")
+                choices.append(Separator(last_date))
+            num_free = 0
+            for att in freebusy:
+                status = freebusy[att][time][2]
+                char = {
+                    'oof': '▓',
+                    'busy':'█',
+                    'tentative': '░',
+                    'free': ' '
+                }.get(status, '?')
+                if status not in busy_statuses:
+                    num_free +=1
+                line +=  char * 5 + '│'
+            line +=   f'{num_free:2d}/{len(freebusy)} ' + num_free * '█'
+            if (num_free + 0.5) / len(freebusy) * 100 > rate:
+                choices.append({"name":line, "value": (time, time.shift(minutes=lenght))})
+            else:
+                choices.append(Separator(line))
+
+        res = prompt(questions=[
+            {
+                "type": "list",
+                "message": "Select a Timeslot to create a meeting (busy='█', free=' '):\n            " + ' '.join([key[:5] for key in freebusy]),
+                "choices": choices,
+                "long_instruction": f"You can choose slots with {rate}% rate. Use '--rate 50' parameter to select slots with partial availability."
+            },
+        ])
+
+    else:
+        # show only slots where everybody is available
+        slots = {}
+
+        for time in list(freebusy.values())[0]:
+            num_free = 0
+            busy_attendees = []
+            for att in freebusy:
+                free = True
+                if freebusy[att][time][2] in busy_statuses: free = False
+                if time.format('HHmm') > between_upper or time.format('HHmm') < between_lower: free = False
+                if time.format('ddd') in ("Sat", "Sun"): free = False
+                if free:
+                    num_free += 1
+                else:
+                    busy_attendees.append(att)
+            busy_attendees = sorted(busy_attendees)
+
+            # do not show domains if they are all the same
+            domains = [email.split('@')[1] for email in busy_attendees]
+            if len(set(domains)) == 1:
+                busy_attendees = [email.split('@')[0] for email in busy_attendees]
+
+
+
+            if num_free == len(freebusy):
+                slot_name = f'{time.format(fmt)} - {time.shift(minutes=lenght).format(fmt)} {str(LOCAL_TIMEZONE)}'
+                all_tz_free = True
+                # for tz in alternative_tz:
+                #     # print(f'################ tz: {tz}  #####################')
+                #     time_start_tz = current_time_start.to('local').to(tz)
+                #     time_end_tz = time.to(tz)
+                #     if time_end_tz.format('HHmm') > between_upper or time_start_tz.format('HHmm') < between_lower:
+                #         all_tz_free = False
+
+
+            # check if this time ends a started slot
+            # fixme: a change in busy_attendees should also trigger a slot end, but in this case the slot can be splitted in 2. 
+            if (num_free + 0.5) / len(freebusy) * 100 <= rate \
+               or (current_slot_status == "started" and current_busy_attendees != busy_attendees): 
+                if current_slot_status == "started" and current_time_start.shift(minutes=lenght - 1) < time:
+                    slot_name = '%20s' % current_time_start.format("dddd DD MMMM " + fmt) + ' - '  + time.format(fmt) + " " + str(LOCAL_TIMEZONE)
+                    all_tz_free = True
+                    for tz in alternative_tz:
+                        # print(f'################ tz: {tz}  #####################')
+                        time_start_tz = current_time_start.to('local').to(tz)
+                        time_end_tz = time.to(tz)
+                        if time_end_tz.format('HHmm') > between_upper or time_start_tz.format('HHmm') < between_lower:
+                            all_tz_free = False
+                        slot_name += ' / '  +  time_start_tz.format(fmt) + ' - '  + time_end_tz.to(tz).format(fmt) + " " + str(tz)
+                    if current_busy_attendees:
+                        slot_name += ' (N/A: ' + ','.join(current_busy_attendees) + ')'
+                    if all_tz_free:
+                        slots[slot_name] = (current_time_start, time)
+                current_time_start = None
+                current_slot_status = "not started"
+            if (num_free + 0.5) / len(freebusy) * 100 >  rate:
+                if not current_time_start:
+                    current_time_start = time
+                    current_busy_attendees = busy_attendees
+                current_slot_status = "started"
+
+
+
+        res = prompt(questions=[
+            {
+                "type": "checkbox",
+                "message": "Select a Timeslot to create a meeting (space to multi-select, enter to finish):",
+                "choices": [{"name": "Cancel (CTRL+C)", "value": None}] + [Separator()]
+                        + [{"name":slots_name, "value": slots_data} for slots_name, slots_data in slots.items()],
+                "default": "default",
+            },
+        ])
+
+
+    if res[0] is None: 
+        return
+
+    for i, (start_time, end_time) in enumerate(res[0]):
+        print(f'######### Creating Meeting {i}  ##########')
+        start_time= start_time.format()[:-9]
+        print(f'start time: {str(start_time)}')
+        outlook.create_event(
+            subject='Placeholder',
+            content='Hello,\n',
+            attendees=list(freebusy.keys()),
+            # start_time='2022-02-03 12:00',#start_time.format(),
+            start_time=start_time.format(),#[:-5],
+            end_time=end_time,
+        )
+
+    # if inquirer.select(
+    #         message="event created. are you happy with this event?",
+    #         choices=['yes, thank you', 'no, delete event'],
+    #     ).execute() == 'no, delete event':
+    #     event.delete()
+
+
+
+if __name__ == '__main__':
+    main()
